16cf8731f965a0a3bfa5d4f1903ac7a6092f9995
Spencer Eldred <eldredspencer@gmail.com>
2013-11-12 13:30:45 -0800

    Update readme and add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,9 @@
 TeamProject::Application.routes.draw do
 
   root :to => 'books#index'
-
-  get "lists/index"
-
-  get "lists/create"
-
-  get "lists/new"
-
-  get "books/index"
-
-  get "books/create"
-
-  get "books/new"
-
-  get "books/destroy"
-
-  get "users/index"
-
-  get "users/new"
-
-  get "users/create"
-
-
+  resources :users
+  resources :books
+  resources :lists
 
 
 end
